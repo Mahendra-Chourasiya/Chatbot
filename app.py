f14b70575771d63db33341638a9285e125c08a12
Mahendra-Chourasiya <96823758+Mahendra-Chourasiya@users.noreply.github.com>
2024-08-16 13:26:46 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,18 +1,11 @@
 import streamlit as st
-import openai
 from langchain_openai import ChatOpenAI
 from langchain_core.output_parsers import StrOutputParser
 from langchain_core.prompts import ChatPromptTemplate
-import os
 
-import os
-from dotenv import load_dotenv
-load_dotenv()
-
-## Langsmith Tracking
-os.environ["LANGCHAIN_API_KEY"]=os.getenv("LANGCHAIN_API_KEY")
-os.environ["LANGCHAIN_TRACING_V2"]="true"
-os.environ["LANGCHAIN_PROJECT"]="Simple Q&A Chatbot With OPENAI"
+# ## Langsmith Tracking
+# os.environ["LANGCHAIN_TRACING_V2"]="true"
+# os.environ["LANGCHAIN_PROJECT"]="Simple Q&A Chatbot With OPENAI"
 
 ## Prompt Template
 prompt=ChatPromptTemplate.from_messages(
@@ -41,7 +34,7 @@ st.sidebar.title("Settings")
 api_key=st.sidebar.text_input("Enter your Open AI API Key:",type="password")
 
 ## Select the OpenAI model
-engine=st.sidebar.selectbox("Select Open AI model",["gpt-4o","gpt-4-turbo","gpt-4"])
+engine=st.sidebar.selectbox("Select Open AI model",["gpt-3.5-turbo"])
 
 ## Adjust response parameter
 temperature=st.sidebar.slider("Temperature",min_value=0.0,max_value=1.0,value=0.7)
